b3588a344255db96b0c8172af4da8f643866bb0f
pro-D-coder <2019pietcsdaksh49@poornima.org>
2021-04-06 22:25:43 +0530

    ADDed some Code

--- C_P.py
+++ C_P.py
@@ -1,31 +1,65 @@
 class Batsman:
-    def __init__(self, name =  None, j_num = None, score = 0, status = True):
+    def __init__(self, name =  None, j_num = None, score = 0, status = True, played_bowls = 0):
         self.name = name
         self.j_num = j_num
         self.score = score
         self.status = status
+        self.played_bowls = played_bowls
     def get_score(self):
         return self.score
+    def get_played_bowls(self):
+        return self.played_bowls
     def get_name(self):
         return self.name
     def get_status(self):
         return self.status
     def get_j_num(self):
         return self.j_num
-    def update_score(self, to_add):
+    def update_score(self, to_add = 1):
         self.score += to_add
     def change_status(self, state):
         self.status = state
 
 class bowler:
-    def __init__(self, name = None, j_num = None, runs = None, wickets = 0, over = 0, balls = 0):
-        self.name = name
-        self.j_num = j_num
-        self.runs = runs
+    def __init__(self, name = None, j_num = None, runs_gived = None, wickets = 0, over = 0, balls = 0, Team):
+        self.bow_name = name
+        self.bow_j_num = j_num
+        self.runs_gived = runs_gived
         self.wickets = wickets
-        self.over = over
-        self.balls =  balls
-    def get_name(self):
-        return self.name
-    def get_j_num(self):
-        return self.j_num
+        self.bow_over = over
+        self.bow_balls =  balls
+        self.Team = Team
+    def get_bow_name(self):
+        return self.bow_name
+    def get_bow_Team(self):
+        return self.Team
+    def get_bow_j_num(self):
+        return self.bow_j_num
+    def get_runs_gived(self):
+        return self.runs_gived
+    def get_wickets(self):
+        return self.wickets
+    def get_bow_over(self):
+        return self.bow_over
+    def get_bow_balls(self):
+        return self.bow_balls
+    def update_runs_gived(self, runs = 1):
+        self.runs_gived += runs
+    def update_wickets(self, wickets = 1):
+        self.wickets += wickets
+    def update_overs(self, overs = 1):
+        self.bow_over += overs
+    def update_balls(self, ball = 1):
+        self.bow_balls += balls
+class Team:
+    NUMBER_OF_TEAMS = 0
+    def __init__(self, id = NUMBER_OF_TEAMS+1, name = None, members = 11):
+        Team.NUMBER_OF_TEAMS += 1
+        self.team_id = id
+        self.team_name = name
+        self.team_members = members
+    def __repr__(self):
+        return self.team_name
+
+c = Team()
+print(c.NUMBER_OF_TEAMS)
