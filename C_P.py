828818f837f3e1bd27145a14021d719358ddfe8a
pro-D-coder <2019pietcsdaksh49@poornima.org>
2021-04-06 20:50:44 +0530

    Added some code

--- C_P.py
+++ C_P.py
@@ -1,11 +1,13 @@
 class Batsman:
-    def __init__(self, name, j_num, score = 0, status = True):
+    def __init__(self, name =  None, j_num = None, score = 0, status = True):
         self.name = name
         self.j_num = j_num
         self.score = score
         self.status = status
     def get_score(self):
         return self.score
+    def get_name(self):
+        return self.name
     def get_status(self):
         return self.status
     def get_j_num(self):
@@ -16,4 +18,14 @@ class Batsman:
         self.status = state
 
 class bowler:
-    def __init__(self, )
+    def __init__(self, name = None, j_num = None, runs = None, wickets = 0, over = 0, balls = 0):
+        self.name = name
+        self.j_num = j_num
+        self.runs = runs
+        self.wickets = wickets
+        self.over = over
+        self.balls =  balls
+    def get_name(self):
+        return self.name
+    def get_j_num(self):
+        return self.j_num
