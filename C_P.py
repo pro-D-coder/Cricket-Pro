b0a1dc42fb8320f71633568ed3d25ee5f71210af
pro-D-coder <2019pietcsdaksh49@poornima.org>
2021-04-08 18:14:42 +0530

    Added some code

--- C_P.py
+++ C_P.py
@@ -1,25 +1,27 @@
 class bowler_stats:
-    def __init__(self, over = 0, balls = 0, runs_gived = 0, wickets = 0):
+    def __init__(self, over = 0, balls = 0, runs_gived = 0, wickets = 0, economy = 0):
         self.over = over
         self.balls = balls
         self.runs_gived = runs_gived
         self.wickets = wickets
-
+        self.economy = economy
 class batsman_stats:
     def __init__(self, runs = 0, played_bowls = 0, strike_rate = 100, status = True):
         self.runs = runs
         self.played_bowls = played_bowls
         self.strike_rate = strike_rate
         self.status = status
-
-class Player(bowler_stats, batsman_stats):
+class all_rounder_stats(bowler_stats, batsman_stats):
+    pass
+class Player(all_rounder_stats):  
     player_count = 0
-    def __init__(self, name, j_num):
+    def __init__(self, j_num, name = "", type = 'bat'):  #available Types are bat: batsman, bow: bowler, all: all-rounder.
         self.name = name
         self.j_num = j_num
-        self.team = Team()
-
-class Team(Bowler, Batsman):
+        self.type = type
+    def __str__(self):
+        return self.name
+class Team():
     NUMBER_OF_TEAMS = 0
     def __init__(self, id = NUMBER_OF_TEAMS+1, name = "Player", n_members = 11, ):
         Team.NUMBER_OF_TEAMS += 1
@@ -33,13 +35,20 @@ class Team(Bowler, Batsman):
         return self.team_name
     def get_n_member(self):
         return self.n_team_members
-    def get_players_name(self):
+    def take_players_name(self):
+        print("Enter Player Name Inorder they bat: ")
         for i in range(1, self.n_team_members+1):
-            self.team_members.append(input("Enter {0} Player Name: ".format(i)))
+            player =  Player(name = input("Enter {0} player name: ".format(i)), j_num = input("Enter player jersey number: "), input("Enter Player Type(bat/bow/all): "))
+            self.team_members.append(player)
 
-class Cricket(Team):
+class Cricket():
     def __init__(self):
         print("\t\t*********************WEL-COME TO CRICKET BOARD******************")
-        self.teamA =  super().__init__(name = input("Enter First Team Name: "), n_members= int(input('Enter Number of Members: ')))
-        self.teamB = super().__init__(name= input('Enter Second Team Name: '), n_members=int(input("Enter Number of Members: ")))        
+        self.teamA = Team(name = input("Enter First Team Name: "), n_members= int(input('Enter Number of Members: ')))
+        self.teamA.take_players_name()
+        self.teamB = Team(name = input("Enter First Team Name: "), n_members= int(input('Enter Number of Members: ')))
+        self.teamB.take_players_name()
+
+        
+
 match = Cricket()
