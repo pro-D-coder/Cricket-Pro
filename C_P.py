cdf3c293680b2db85b3448d628dbe2a3e32fba10
pro-D-coder <2019pietcsdaksh49@poornima.org>
2021-04-10 02:11:17 +0530

    Added some Code

--- C_P.py
+++ C_P.py
@@ -1,12 +1,23 @@
+from os import system, name
+def clear():
+  
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+  
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
 def set_stats(type):
     if type == "bat":
         bat_stats = batsman_stats()
         return bat_stats
-    elif type == "bat":
+    elif type == "bow":
         bowl_stats = bowler_stats()
         return bowl_stats
     else:
-        bowl_stats = bowler_stats()
+        all_stats = all_rounder_stats()
         return bowl_stats
 class score_card:
     def __init__(self, wickets = 0, runs = 0):
@@ -18,9 +29,8 @@ class over:
         self.over = str(balls//6) +"."+ str(balls%6)
     def __repr__(self):
         return self.over
-
 class bowler_stats:
-    def __init__(self, over, runs_gived = 0, wickets = 0, economy = 0):
+    def __init__(self, over = over(), runs_gived = 0, wickets = 0, economy = 0):
         self.over = over
         self.runs_gived = runs_gived
         self.wickets = wickets
@@ -44,7 +54,7 @@ class Player():
         return self.name
 class Team():
     NUMBER_OF_TEAMS = 0
-    def __init__(self, id = NUMBER_OF_TEAMS+1, name = "Player", n_members = 11, ):
+    def __init__(self, id = NUMBER_OF_TEAMS+1, name = "", n_members = 11, ):
         Team.NUMBER_OF_TEAMS += 1
         self.team_id = id
         self.team_name = name
@@ -57,16 +67,29 @@ class Team():
     def get_n_member(self):
         return self.n_team_members
     def take_players_name(self):
-        print("Enter Player Name Inorder they bat: ")
+        print("Enter Player Detail Inorder they bat: ")
         for i in range(1, self.n_team_members+1):
             player =  Player(j_num = input("Enter player jersey number: "), name = input("Enter {0} player name: ".format(i)), type = input("Enter Player Type(bat/bow/all): "))
             self.team_members.append(player)
 
-def display():
-    print()
+def display(match, teamA, teamB, ):
+    clear()
+    print("Team: {0}", teamA.team_name)
+    print("Score: {0} / {1}\n".format(match.score_card.runs, match.score_card.wickets))
+    print("Over: {0}".format(match.over))
+    print("Batsman:  {0}  {1}{2}\t\t{3} {4}{5}".format())
+    print("Bowler: {0}({1})".format())
+    
+
 class Cricket():
     def __init__(self, total_overs = over(), score = score_card()):
         self.over = total_overs
         self.score_card = score
 
-match = Cricket()
+print("***************************WEL-COME IN CRICKET BOARD*****************************")
+match = Cricket(over(balls=  int(input("Enter Number Of Balls To Be Played Per Inning: "))), score = score_card())
+teamA = Team(name = input("Enter First Team Name: "), n_members= int(input("Enter Number Of Team Members: ")))
+teamA.take_players_name()
+teamB = Team(name = input("Enter Second Team Name: "), n_members= int(input("Enter Number Of Team Members: ")))
+teamB.take_players_name()
+display(match, teamA, teamB)
